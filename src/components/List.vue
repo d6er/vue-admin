6fc9cae7683c847e3cc9887252994f3aa79fd5a1
d6er <d6er@qq.com>
2017-05-22 00:06:29 +0900

    Bulma

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="list">
-    <table class="table">
+    <table class="table is-narrow">
       <thead>
         <th>ID</th>
+        <th>Img</th>
         <th>Title</th>
         <th>Status</th>
         <th>Updated</th>
       </thead>
       <tbody>
         <tr v-for="item in items">
-          <td>{{ item.id }}</td>
+          <td>
+            <input type="checkbox">
+          </td>
+          <td>
+            <figure class="image is-32x32">
+              <img src="http://bulma.io/images/placeholders/32x32.png">
+            </figure>
+          </td>
           <td>
             <router-link :to="'/item/' + item.id">{{ item.title }}</router-link>
+            <span class="tag is-dark">active</span>
           </td>
           <td>
           </td>
