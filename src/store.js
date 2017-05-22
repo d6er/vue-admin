921b6f37ab526e74214b575ca1512f8f6d437e56
d6er <d6er@qq.com>
2017-05-22 23:39:18 +0900

    ssr api

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import ws from './websocket'
+import api from 'api'
 
 Vue.use(Vuex)
 
@@ -8,20 +8,13 @@ export default new Vuex.Store({
   state: {
     user: null,
     isAuthenticated: false,
-    items: [
-      { id: 1, title: 'item foo' },
-      { id: 2, title: 'item bar' },
-      { id: 101, title: 'item title title' },
-      { id: 110, title: 'item bird foo' },
-      { id: 123, title: 'item foo boo' },
-      { id: 234, title: 'item boost boost' }
-    ]
+    items: []
   },
   actions: {
     
     create_account ({ commit }, data) {
       return new Promise((resolve, reject) => {
-        ws.send('create_account', data).then(response => {
+        api.call('createAccount', data).then(response => {
           // update state
           resolve(response)
         }, error => {
@@ -30,9 +23,9 @@ export default new Vuex.Store({
       })
     },
     
-    save_item ({ commit }, data) {
+    saveItem ({ commit }, data) {
       return new Promise((resolve, reject) => {
-        ws.send('save_item', data).then(response => {
+        api.call('saveItem', data).then(response => {
           // update state
           resolve(response)
         }, error => {
@@ -41,18 +34,20 @@ export default new Vuex.Store({
       })
     },
     
-    fetch_items ({ commit }, data) {
-      //return api.fetchItems(data).then(r => {
-      // console.dir(r)
-      //})
-      console.log('store.js fetch_items')
-      return ws.send('fetch_items', data).then(r => {
-        console.dir(r)
+    fetchItems ({ commit }, data) {
+      return api.call('fetchItems', data).then(items => {
+        console.dir(items)
+        commit('setItems', items)
       })
     }
   },
   strict: true,
   mutations: {
-    
+    // todo: use constant for function names. https://vuex.vuejs.org/en/mutations.html
+    setItems (state, items) {
+      console.log('setItems')
+      console.dir(items)
+      state.items = items
+    }
   }
 })
