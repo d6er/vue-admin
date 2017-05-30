3510a46bbefcb26a14a986f1e5b81df766b754df
d6er <d6er@qq.com>
2017-05-30 23:57:44 +0900

    side menu

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,16 +1,38 @@
 <template>
   <aside class="menu">
+    <p class="menu-label">
+      ITEMS
+    </p>
     <ul class="menu-list">
       <li>
-        <router-link to="/list">
-          Active
+        <router-link to="/items/published">
+          Published
           <span class="tag">123</span>
         </router-link>
       </li>
-      <li><a>Unsold</a></li>
-      <li><a>Drafts</a></li>
-      <li><a>Scheduled</a></li>
-      <li><a>Ended</a></li>
+      <li>
+        <router-link to="/items/unpublished">
+          Unpublished
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/items/draft">
+          Draft
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/items/trash">
+          Trash
+        </router-link>
+      </li>
+      <li>
+        <hr/>
+      </li>
+      <li>
+        <router-link to="/items/trash">
+          Setting
+        </router-link>
+      </li>
     </ul>
   </aside>
 </template>
