042fcae0d48b543b617a1bf00c6369b5d35b9e49
d6er <d6er@qq.com>
2017-09-06 14:04:21 +0900

    FilterTree

--- src/views/FilterTree.vue
+++ src/views/FilterTree.vue
@@ -3,23 +3,21 @@
     <template v-for="filter in filters">
       <li v-if="!filter.foreach">
         <router-link :to="getFilterUrl(filter.name)"
-                     :class="{ 'is-active': isActive(parentPath + '/' + encodeURIComponent(filter.name)) }"
+                     :class="{ 'is-active': isActive(filter.name) }"
                      class="is-capitalized">
           {{ filter.name }}
         </router-link>
         <FilterTree v-if="hasChildren(filter.name) && filter.name == path[depth]"
-                    :arrPath="[...arrPath, filter.name]"
-                    :depth="parseInt(depth) + 1"/>
+                    :arrPath="[...arrPath, filter.name]"/>
       </li>
       <li v-if="filter.foreach != ''" v-for="elm in $store.state[filter.foreach]">
         <router-link :to="getFilterUrl(filter.name + ':' + elm)"
-                     :class="{ 'is-active': isActive(parentPath + '/' + filter.name + ':' + elm) }"
+                     :class="{ 'is-active': isActive(filter.name + ':' + elm) }"
                      class="is-capitalized">
           {{ elm }}
         </router-link>
         <FilterTree v-if="hasChildren(filter.name) && filter.name + ':' + elm == path[depth]"
-                    :arrPath="[...arrPath, filter.name + ':' + elm]"
-                    :depth="parseInt(depth) + 1"/>
+                    :arrPath="[...arrPath, filter.name + ':' + elm]"/>
       </li>
     </template>
   </ul>
@@ -35,29 +33,24 @@ export default {
     arrPath: {
       type: Array,
       default: () => []
-    },
-    parentPath: {
-      type: String,
-      default: ''
-    },
-    depth: {
-      type: Number,
-      default: 0
     }
   },
   
   computed: {
-    list() {
+    depth () {
+      return this.arrPath.length
+    },
+    list () {
       return this.$store.state.lists.find(list => list.name == this.$route.params.list)
     },
-    filters() {
+    filters () {
       let parentFilterName = ''
       if (this.arrPath.length) {
         parentFilterName = this.arrPath[this.arrPath.length-1].replace(/:.+$/, '')
       }
       return this.list.filters.filter(filter => filter.parent == parentFilterName)
     },
-    path() {
+    path () {
       return this.$route.params.filter.split(',')
     }
   },
@@ -69,9 +62,8 @@ export default {
     hasChildren(filterName) {
       return this.list.filters.some(filter => filter.parent == filterName)
     },
-    isActive(path) {
-      // todo: bug when paging
-      return this.$route.path == path
+    isActive(filterName) {
+      return [ ...this.arrPath, filterName ].join(',') == this.$route.params.filter
     }
   }
 }
