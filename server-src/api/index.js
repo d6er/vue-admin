f3f5b1ae5d936d34652a5390eb47e5535a3b20fa
d6er <d6er@qq.com>
2017-11-05 23:28:26 +0900

    cleaning

--- server-src/api/index.js
+++ server-src/api/index.js
@@ -5,12 +5,4 @@ const hackerNews = require('./hacker-news')
 
 const methods = Object.assign({}, user, account, item)
 
-methods.initialize = () => {
-  console.log('server api initialize')
-  return new Promise((resolve, reject) => {
-    resolve({ text: 'foo' })
-    resolve({ text: 'bar' })
-  })
-}
-
 module.exports = methods
