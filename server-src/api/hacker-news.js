b20dc2a0ce1d37016d3e4a880b40f9d2f11071bf
d6er <d6er@qq.com>
2017-11-05 23:41:09 +0900

    hacker news

--- server-src/api/hacker-news.js
+++ server-src/api/hacker-news.js
@@ -9,10 +9,10 @@ const methods = {
   
   syncTopStories: (user_id) => {
     
+    wsPool.send(user_id, 'Syncing hacker news top stories.')
+    
     let url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
     
-    wsPool.send(user_id, 'syncing top stories.')
-
     return axios.get(url).then(r => {
       return Promise.all(
         r.data.map((id, idx) => {
@@ -22,7 +22,7 @@ const methods = {
             let item = res.data
             item._id = item.id
             item.idx = idx
-            wsPool.send(user_id, item.title)
+            wsPool.send(user_id, idx + ' - ' + item.title)
             
             return db.collection('hacker-news.' + user_id).updateOne({ _id: item._id },
                                                                      { $set: item},
